3c130bd01bbb4115dca569e15740fb65d456d574
madani93 <madani@citynet.ir>
2024-03-03 20:52:12 +0330

    add style to register form

--- pages/register.vue
+++ pages/register.vue
@@ -3,28 +3,26 @@
       <div class="container">
         <div class="columns">
           <div class="column is-4 is-offset-4">
-            <h2 class="title has-text-centered">Register!</h2>
+            <h2 class="title text-center mt-4">Register!</h2>
   
            
-            <form @submit.prevent="submitForm">
-                <div v-for="(field, index) in fields" :key="index" class="field">
-                <label class="label">{{ field.label }}</label>
+            <form class="flex flex-col justify-center items-center" @submit.prevent="submitForm">
+                <div v-for="(field, index) in fields" :key="index" class="field my-2">
+                <label class="text-m text-gray-500">{{ field.label }}</label>
                 <div class="control">
                     <component
                     :is="field.type"
                     v-model="field.value"
                     :name="field.name"
                     required
+                    class="px-2 py-1 border border-grey-800 rounded-md min-w-64"
                     />
                 </div>
                 </div>
-                <div class="control">
-                <button type="submit" class="button is-dark is-fullwidth">Register</button>
+                <div class="control min-w-64">
+                <button type="submit" class="w-full btn bg-blue-500 mt-3 border rounded-md px-3 py-2 w-full">Register</button>
                 </div>
             </form> 
-            <!-- <div class="has-text-centered" style="margin-top: 20px">
-              Already got an account? <nuxt-link to="/login">Login</nuxt-link>
-            </div> -->
           </div>
         </div>
       </div>
